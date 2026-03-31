a4b528c0802be4088fc05ecd023c90cc8c9fbb81
Ka <kaakaa300@gmail.com>
2026-03-31 21:43:23 +0700

    fea: added nav bar in each page

--- app/page.js
+++ app/page.js
@@ -1,20 +1,26 @@
+import Navbar from "@/components/Navbar";
 import ProductCard from "@/components/ProductCard";
 
 async function getProducts() {
   //Do API thầy cung cấp em không sử dụng được nên em sử dụng một API khác tương tự
-  const res = await fetch("https://fakestoreapiserver.reactbd.org/api/products");
+  const res = await fetch(
+    "https://fakestoreapiserver.reactbd.org/api/products",
+  );
   const result = await res.json();
   return result.data;
 }
 export default async function HomePage() {
   const products = await getProducts();
   // ... render ...
-  console.log(products)
+  console.log(products);
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-6">
-      {products.map((item) => (
-        <ProductCard key={item._id} product={item} />
-      ))}
+    <div>
+      <Navbar></Navbar>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-6">
+        {products.map((item) => (
+          <ProductCard key={item._id} product={item} />
+        ))}
+      </div>
     </div>
   );
 }
